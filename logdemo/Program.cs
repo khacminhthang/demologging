4bdfe548fffd6d636e06ccad48c4161830ce05b5
thanglkm <thanglkm@vnpt.vn>
2024-12-31 11:05:16 +0700

    Initial commit

---
+++ logdemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Cấu hình Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+Log.Information("Application has started.");
+
+app.MapGet("/", () =>
+{
+    Log.Information("Processing request in root endpoint.");
+    return "Hello, Serilog!";
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
